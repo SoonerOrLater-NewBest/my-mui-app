fada4ca9e36130608b89ddd7acf972d3c09bc411
SoonerOrLater-NewBest <1245515178@qq.com>
2018-04-09 14:49:05 +0800

    兼容安卓rem布局及Object.assign()用mui.extend回退处理，兼容低版本

--- common/common.js
+++ common/common.js
@@ -1,22 +1,68 @@
 
 //动态设置根元素尺寸
-(function (doc, win) {
-    var docEl = doc.documentElement,
-        resizeEvt = 'onorientationchange' in window ? 'onorientationchange' : 'resize',
-        recalc = function () {
-            var clientWidth = docEl.clientWidth;
-            if (!clientWidth) return;
-            if(clientWidth>=750){
-                docEl.style.fontSize = '100px';
-            }else{
-                docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
-            }
-        };
+// (function (doc, win) {
+//     var docEl = doc.documentElement,
+//         resizeEvt = 'onorientationchange' in window ? 'onorientationchange' : 'resize',
+//         recalc = function () {
+//             var clientWidth = docEl.clientWidth;
+//             if (!clientWidth) return;
+//             if(clientWidth>=750){
+//                 docEl.style.fontSize = '100px';
+//             }else{
+//                 docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+//             }
+//         };
 
-    if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvt, recalc, false);
-    doc.addEventListener('DOMContentLoaded', recalc, false);
-})(document, window);
+//     if (!doc.addEventListener) return;
+//     win.addEventListener(resizeEvt, recalc, false);
+//     doc.addEventListener('DOMContentLoaded', recalc, false);
+// })(document, window);
+;(function(designWidth, maxWidth) {
+	var doc = document,
+	win = window,
+	docEl = doc.documentElement,
+	remStyle = document.createElement("style"),
+	tid;
+
+	function refreshRem() {
+		var width = docEl.getBoundingClientRect().width;
+		maxWidth = maxWidth || 540;
+		width>maxWidth && (width=maxWidth);
+		var rem = width * 100 / designWidth;
+		remStyle.innerHTML = 'html{font-size:' + rem + 'px;}';
+	}
+
+	if (docEl.firstElementChild) {
+		docEl.firstElementChild.appendChild(remStyle);
+	} else {
+		var wrap = doc.createElement("div");
+		wrap.appendChild(remStyle);
+		doc.write(wrap.innerHTML);
+		wrap = null;
+	}
+	//要等 viewport 设置好后才能执行 refreshRem，不然 refreshRem 会执行2次；
+	refreshRem();
+
+	win.addEventListener("resize", function() {
+		clearTimeout(tid); //防止执行两次
+		tid = setTimeout(refreshRem, 300);
+	}, false);
+
+	win.addEventListener("pageshow", function(e) {
+		if (e.persisted) { // 浏览器后退的时候重新计算
+			clearTimeout(tid);
+			tid = setTimeout(refreshRem, 300);
+		}
+	}, false);
+
+//	if (doc.readyState === "complete") {
+//		doc.body.style.fontSize = "16px";
+//	} else {
+//		doc.addEventListener("DOMContentLoaded", function(e) {
+//			doc.body.style.fontSize = "16px";
+//		}, false);
+//	}
+})(750, 750);
 
 //子页面原生头样式
 var titleNView = { //原生导航配置
